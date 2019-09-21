63d2cbd2637e0e87ad954413411c4df9ae20c378
Munajaf <munajaf.sitegiant@gmail.com>
2019-09-21 13:35:51 +0800

    initial commit
    Signed-off-by: Munajaf <munajaf.sitegiant@gmail.com>

---
+++ resources/js/frontend/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <i class="fas fa-code"></i> Example Vue Component
+        </div>
+        <div class="card-body">
+            hahahaha!
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
